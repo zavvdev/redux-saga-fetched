a10f3b863339d39fe096ae9c0528989b349b1567
zavvdev <zaviryukha.igor99@gmail.com>
2024-10-29 23:25:34 +0200

    init

--- src/02-config/general.ts
+++
@@ -1,7 +0,0 @@
-export const DEFAULT_STALE_TIME_MS = 5 * 60 * 1000;
-
-export const MESSAGES = {
-  error: {
-    domainIsString: "InitOptions.domain must be a string",
-  },
-};
---
+++ src/__tests__/sample.test.js
@@ -0,0 +1,5 @@
+import { expect, test } from "vitest";
+
+test("should not fail", () => {
+  expect(1 === 1).toBe(true);
+});
