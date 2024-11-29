a2a9f3bff7095f2b0b3466fd17d6167ffe7db02b
zavvdev <zaviryukha.igor99@gmail.com>
2024-11-29 22:45:14 +0200

    added d.ts file

---
+++ src/index.d.ts
@@ -0,0 +1,78 @@
+export interface QueryOptions {
+  staleTime: number;
+  extractError?: <T extends Error>(e: T) => any;
+  retry?: number;
+  retryDelay?: number;
+}
+
+export interface MutationOptions {
+  extractError?: <T extends Error>(e: T) => any;
+  retry?: number;
+  retryDelay?: number;
+}
+
+export type Key = (string | number | boolean | bigint)[];
+
+export interface QueryRecord {
+  isLoading: boolean;
+  isFetching: boolean;
+  isLoaded: boolean;
+  isError: boolean;
+  isValid: boolean;
+  isReset: boolean;
+  timestamp: number | undefined;
+  data: unknown;
+  error: unknown | null;
+}
+
+export interface MutationRecord {
+  isLoading: boolean;
+  isLoaded: boolean;
+  isError: boolean;
+  data: unknown;
+  error: unknown | null;
+}
+
+type State = Record<string, QueryRecord | MutationRecord>;
+
+interface Modules {
+  reducer: (
+    state: State,
+    action: {
+      type: string;
+      payload: Record<string, unknown>;
+    },
+  ) => State;
+
+  selector: (
+    key: Key,
+  ) => (state: State) => QueryRecord | MutationRecord;
+
+  query: <T extends unknown>(args: {
+    key: Key;
+    fn: () => Promise<T> | T;
+    options?: Partial<QueryOptions>;
+  }) => Generator<T, T, unknown>;
+
+  mutation: <T extends unknown>(args: {
+    key: Key;
+    fn: () => Promise<T> | T;
+    options?: Partial<MutationOptions>;
+  }) => Generator<T, T, unknown>;
+
+  invalidate: (key: Key) => void;
+
+  reset: (key: Key) => void;
+}
+
+declare function initSagaQuery(args: {
+  domain: string;
+  extractError?: <T extends Error>(error: T) => any;
+  retry?: number;
+  retryDelay?: number;
+  query: QueryOptions;
+  mutation?: MutationOptions;
+  createInstanceId?: () => string;
+}): Modules;
+
+export { initSagaQuery };
